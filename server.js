826d87760f139ecb5346d0e40edd6de03d4507a1
Nabil Fannane <nabil@goadroit.com>
2020-05-23 16:20:59 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const graphqlHTTP=require('express-graphql')
+const app = express()
+
+app.use("/graphql", graphqlHTTP({
+    
+}))
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running ${PORT}`);
+    
+})
